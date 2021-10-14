b893f704e13a40dcae35b853ca334d819f1a8747
miledesmam <miledesmam@gmail.com>
2021-10-13 22:41:35 -0500

    config calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import {Calendar, momentLocalizer} from "react-big-calendar"
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import { Navbar } from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { messages } from "../../helpers/calendar-messages-es";
 
-import { Navbar } from '../ui/Navbar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import "moment/locale/es-mx";
+moment.locale("es");
 
-const localizer = momentLocalizer(moment)
-const events = [{
-  title: 'Cumpleaños Cherc',
-  start: moment().toDate(),
-  end: moment().add(2, 'hours').toDate(),
-  bgcolor: '#fafafa'
-}]
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    title: "Cumpleaños Cherc",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+    notes: 'Comprar pastel'
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    }
+    return{
+      style
+    }
+  }
   return (
-    <div className='calendar-screen'>
-      <Navbar/>
-      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
+    <div className="calendar-screen">
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
-  )
-}
+  );
+};
