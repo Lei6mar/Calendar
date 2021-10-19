f0f745802f7a7e7f8f445690d48e98f9daebaaa4
miledesmam <miledesmam@gmail.com>
2021-10-19 02:14:34 -0500

    envs y authReducer

--- src/types/types.js
+++ src/types/types.js
@@ -2,10 +2,17 @@ export const types = {
   uiOpenModal : '[ui] Open modal',
   uiCloseModal : '[ui] Close modal',
 
-
   eventSetActive: '[event] Set Active',
   eventAddNew: '[event] Add new',
   eventClearActiveEvent: '[event] Clear active event',
   eventUpdated: '[event] Event update',
   eventDeleted: '[event] Event deleted',
+
+  authChecking : '[auth] Checking login state',
+  authCheckingFinished : '[auth] Finished Checking login state',
+  authStartLogin : '[auth] Start login',
+  authLogin : '[auth] Login',
+  authStartRegister : '[auth] Start register',
+  authStartTokenRenew : '[auth] Start token renew',
+  authLogout : '[auth] Logout',
 }
